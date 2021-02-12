5e65a286dcc9a5bcb7f12976e735b8ea60aa3652
jhmoreira <joaomoreirati98@gmail.com>
2021-02-11 21:30:11 -0300

    Adiciona listagem por id

--- src/main/java/com/produtos/api/controller/ProdutoController.java
+++ src/main/java/com/produtos/api/controller/ProdutoController.java
@@ -3,13 +3,16 @@ package com.produtos.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.produtos.api.exception.ProdutoNaoEncontrado;
 import com.produtos.api.model.Produto;
 import com.produtos.api.repository.ProdutoRepositorio;
 
@@ -37,5 +40,11 @@ public class ProdutoController {
 		return produtoRepositorio.save(produto);
 	}
 	
+	//Lista produto pelo ID
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Produto> retornaProdutoPorId(@PathVariable Long id) {
+		Produto produto= produtoRepositorio.findById(id).orElseThrow(()-> new ProdutoNaoEncontrado("Não existe produto com id: "+id));
+		return ResponseEntity.ok(produto);
+	}
 
 }
